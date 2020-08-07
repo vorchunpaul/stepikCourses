44aadb83764fc2dfd3a6056b86bec9d27ae98f05
vorchunpaul <vorchun@tuta.io>
2020-08-07 15:48:55 +0300

    add 2.1.4 && remove sketch

--- haskell1CSC/unit1/sketch.hs
+++
@@ -1,8 +0,0 @@
--- 1.6.9 Integral function
-integration :: (Double -> Double) -> Double -> Double -> Double
-
-integration f a b = h * (x + sum (n-1)) where
-  n = 1000
-  h = (b - a) / n
-  x = (f a + f b) / 2
-  sum n = if n == 0 then 0 else f (a + h * n ) + sum (n - 1)
---
+++ haskell1CSC/unit2/2.1.4.hs
@@ -0,0 +1,3 @@
+-- 2.1.4
+getSecondFrom :: p1 -> p2 -> p3 -> p2
+getSecondFrom a b c = b
